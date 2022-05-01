1368a03bdba9a9046069b77beb76743068e70dc3
Ikhwanul Abiyu Dhiyya'ul Haq <86358803+Soellassy24@users.noreply.github.com>
2022-05-01 10:34:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,12 @@ Iya.addEventListener("click", function () {
         Image.setAttribute("src", "https://c.tenor.com/57JSPYRo0k8AAAAj/lebaran-selamat-lebaran.gif");
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Selamat hari raya Idul Fitri 1443 Hijriah!! Jangan lupa tetap melakukan protokol kesehatan!";
+        Text.innerHTML = "Selamat hari raya Idul Fitri 1443 Hijriah!!";
+        Text.classList.add('text-animate');
+    else if(Iya.value == 9){
+        Text.classList.remove('text-animate');
+        void Text.offsetWidth; 
+        Text.innerHTML = "Jangan lupa tetap memperhatikan protokol kesehatan!!";
         Text.classList.add('text-animate');
         Iya.remove();
     }
